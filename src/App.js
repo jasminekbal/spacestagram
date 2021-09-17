d65b99f9580dc01d866208d9f56ff45aba8223ef
Jasmine Bal <baljasminek@gmail.com>
2021-09-16 20:09:02 -0400

    updated useeffect

--- src/App.js
+++ src/App.js
@@ -4,45 +4,18 @@ import heartEmpty from "./resources/heart.svg"
 import heartFull from "./resources/heartFilled.svg"
 
 
-const pics = [
-  {title: " Picture of the Day",
-    date: "2000-10-11",
-    imageURL: "https://api.nasa.gov/assets/img/general/apod.jpg",
-    desc: "It has the popular appeal of a Justin Bieber video. This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds; but generally help with discoverability of relevant imagery.",
-    liked: false,},
-    {title: " Picture of the Day",
-    date: "2000-11-11",
-    imageURL: "https://api.nasa.gov/assets/img/general/apod.jpg",
-    desc: "It has the popular appeal of a Justin Bieber video. This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds; but generally help with discoverability of relevant imagery.",
-    liked: false,},
-    {title: " Picture of the Day",
-    date: "2000-12-11",
-    imageURL: "https://api.nasa.gov/assets/img/general/apod.jpg",
-    desc: "It has the popular appeal of a Justin Bieber video. This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds; but generally help with discoverability of relevant imagery.",
-    liked: false,},
-    {title: " Picture of the Day",
-    date: "2000-13-11",
-    imageURL: "https://api.nasa.gov/assets/img/general/apod.jpg",
-    desc: "It has the popular appeal of a Justin Bieber video. This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds; but generally help with discoverability of relevant imagery.",
-    liked: false,},
-    {title: " Picture of the Day",
-    date: "2000-14-11",
-    imageURL: "https://api.nasa.gov/assets/img/general/apod.jpg",
-    desc: "It has the popular appeal of a Justin Bieber video. This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds; but generally help with discoverability of relevant imagery.",
-    liked: false,}
-]
-
 
 function App() {
 
-  var [state, setState] = useState({photos: pics});
+  var [state, setState] = useState({photos: []});
 
   useEffect(() => {
     callApi()
     return () => {
       
     }
-  }, )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   async function callApi () {
     const url = `https://api.nasa.gov/planetary/apod?start_date=2021-08-30&api_key=bwToWga6yNLMtKSaU7siWCE0NVGObtnjWFTYNgqm`
